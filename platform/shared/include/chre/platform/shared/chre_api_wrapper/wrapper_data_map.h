dbdbacae1c1dbe1cec4832efcbbad4dbafe2595c
FromLiQg <1501077298@qq.com>
2023-03-22 14:54:29 +0800

    add function signature of delModuleInstanceMap in wrapper_data_map.h

--- platform/shared/include/chre/platform/shared/chre_api_wrapper/wrapper_data_map.h
+++ platform/shared/include/chre/platform/shared/chre_api_wrapper/wrapper_data_map.h
@@ -1,15 +1,24 @@
 #ifndef _WRAPPER_DATA_MAP_H_
 #define _WRAPPER_DATA_MAP_H_
+
 #include <map>
 #include "wasm_export.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-uint32_t getOffset(wasm_module_inst_t WasmModuleInst, void *nativeData);
-bool setNativeData(wasm_module_inst_t WASMModuleInst, void *nativeData, uint32 dataOffset) {
-bool delNativeData(wasm_module_inst_t WasmModuleInst, void *nativeData);
-#ifdef __cplusplus
-}#endif
+/**
+ * Get the offset mapping to nativeData in WASM Module instance
+ * @param WASMModuleInst the WASM module instance to find a offset
+ * @param nativeData the native data to be mapped to the offset
+*/
+uint32_t getOffset(wasm_module_inst_t WASMModuleInst, const void *nativeData);
+bool setNativeData(wasm_module_inst_t WASMModuleInst, const void *nativeData, uint32_t dataOffset);
+bool delNativeData(wasm_module_inst_t WASMModuleInst, const void *nativeData);
+bool delModuleInstanceMap(wasm_module_inst_t WASMModuleInst);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
