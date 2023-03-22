c2409ba637a3390d3e069b99a8b0db3f08974fe6
FromLiQg <1501077298@qq.com>
2023-03-22 13:30:27 +0800

    wrapper_data_map.cc: bugFix

--- platform/shared/chre_api_wrapper/wrapper_data_map.cc
+++ platform/shared/chre_api_wrapper/wrapper_data_map.cc
@@ -7,44 +7,53 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-static std::map<wasm_module_inst_t, std::map<void* ,uint32_t>> eventDataMap;
-uint32_t getOffset(wasm_module_inst_t WASMModuleInst, void *nativeData) {
+static std::map<wasm_module_inst_t, std::map<const void* ,uint32_t> > eventDataMap;
+uint32_t getOffset(wasm_module_inst_t WASMModuleInst, const void *nativeData) {
     uint32_t offset = 0;
     auto moduleMap = eventDataMap.find(WASMModuleInst);
     if (eventDataMap.end() ==  moduleMap) {
-        eventDataMap[WASMModuleInst] = std::map<void* ,uint32_t>();
+        eventDataMap[WASMModuleInst] = std::map<const void* ,uint32_t>();
     }
-    auto result = moduleMap.find(nativeData);
-    if (moduleMap.end() != result) {
+    auto result = moduleMap->second.find(nativeData);
+    if (moduleMap->second.end() != result) {
        offset = result->second; 
     }
     return offset;
 }
-bool setNativeData(wasm_module_inst_t WASMModuleInst, void *nativeData, uint32 dataOffset) {
+bool setNativeData(wasm_module_inst_t WASMModuleInst, const void *nativeData, uint32_t dataOffset) {
     bool state = true;
     auto moduleMap = eventDataMap.find(WASMModuleInst);
     if (eventDataMap.end() ==  moduleMap) {
-        eventDataMap[WASMModuleInst] = std::map<void* ,uint32_t>();
+        eventDataMap[WASMModuleInst] = std::map<const void* ,uint32_t>();
     }
-    auto result = moduleMap.find(nativeData);
-    if (moduleMap.end() != result) {
+    auto result = moduleMap->second.find(nativeData);
+    if (moduleMap->second.end() != result) {
         state = false; 
     } else {
-        moduleMap[nativeData] = dataOffset;
+        moduleMap->second[nativeData] = dataOffset;
     }
     return state;
 }
-bool delNativeData(wasm_module_inst_t WasmModuleInst, void *nativeData) {
+bool delNativeData(wasm_module_inst_t WasmModuleInst, const void *nativeData) {
     bool state = true;
-    auto moduleMap = eventDataMap.find(WASMModuleInst);
+    auto moduleMap = eventDataMap.find(WasmModuleInst);
     if (eventDataMap.end() ==  moduleMap) {
-        eventDataMap[WASMModuleInst] = std::map<void* ,uint32_t>();
+        eventDataMap[WasmModuleInst] = std::map<const void* ,uint32_t>();
     }
-    auto result = moduleMap.find(nativeData);
-    if (moduleMap.end() == result) {
+    auto result = moduleMap->second.find(nativeData);
+    if (moduleMap->second.end() == result) {
         state = false; 
     } else {
-        moduleMap.erase(result);
+        moduleMap->second.erase(result);
+    }
+    return state;
+}
+bool delModuleInstanceMap(wasm_module_inst_t WASMModuleInst) {
+    bool state = false;
+    auto moduleMap = eventDataMap.find(WASMModuleInst);
+    if (eventDataMap.end() !=  moduleMap) {
+        eventDataMap.erase(moduleMap);
+        state = true;
     }
     return state;
 }
